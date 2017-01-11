99a851c073957a60903aaf7cd25cacf0fd7b7369
郭轩 <guoxuan@guoxuan.local>
2017-01-11 10:47:13 +0800

    OK

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,12 +6,6 @@ Page({
     motto: 'Hello World',
     userInfo: {}
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
   onLoad: function () {
     console.log('onLoad')
     var that = this
