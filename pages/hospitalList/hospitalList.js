dfb3bc4090c15203f43e278b38202fc2b19ea2df
郭轩 <guoxuan@guoxuan.local>
2017-01-13 14:39:47 +0800

    OK

--- pages/hospitalList/hospitalList.js
+++ pages/hospitalList/hospitalList.js
@@ -1,41 +1,44 @@
 const HospitalListSvc = require('../../services/hospitalListSvc.js')
 const GLOBAL = require('../../global.js');
+const done = require('../../utils/promise/done.js').done;
 
 Page({
     data: {
         searchArray: [],
-        array: ["北京", "上海"],
+        array: [
+            "北京", "上海"
+        ],
         hosArray: [],
         txt: "地区",
         imgAddress: GLOBAL.SERVER + "/images/",
-        searchKey:""
+        searchKey: ""
     },
-    bindp: function (e) {
-        this.setData({
-            index: e.detail.value
-        })
+    bindp: function(e) {
+        this.setData({index: e.detail.value})
     },
-    onLoad: function () {
+    onLoad: function() {
+        wx.showToast({title: '加载中', icon: 'loading', duration: 10000, mask: true})
         var hospitalSvc = new HospitalListSvc()
         hospitalSvc.getLocation().then(data => {
             this.setData({
-                searchArray: data.data.data,//查询结果
-                hosArray:data.data.data //全部列表
+                searchArray: data.data.data, //查询结果
+                hosArray: data.data.data //全部列表
             })
-        })
+        }).done(() => {
+            console.log('done');
+            wx.hideToast();
+        });
     },
-    searchFunc: function(e){
+    searchFunc: function(e) {
         var list = [];
-        for(var i = 0; i < this.data.hosArray.length; i++){
-            if(this.data.hosArray[i] != null){
-                if(this.data.hosArray[i].name.indexOf(e.detail.value) > 0){
+        for (var i = 0; i < this.data.hosArray.length; i++) {
+            if (this.data.hosArray[i] != null) {
+                if (this.data.hosArray[i].name.indexOf(e.detail.value) > 0) {
                     list.push(this.data.hosArray[i])
                 }
             }
         }
-        this.setData({
-            searchArray: list
-        })
+        this.setData({searchArray: list})
     }
 
 })
--- services/hospitalListSvc.js
+++ services/hospitalListSvc.js
@@ -1,32 +1,31 @@
 const GLOBAL = require('../global.js');
 const request = require('../utils/request.js').request
+const Promise = require('../utils/promise/core.js');
 
-var HospitalListSvc = function (){
+var HospitalListSvc = function() {}
 
-}
-
-HospitalListSvc.prototype.getHospitalList = function (){
+HospitalListSvc.prototype.getHospitalList = function() {
     return new Promise((resolve, reject) => {
-        request(GLOBAL.SERVER + "/hospital/getAll/全部/全部", '' ,"GET").then(data => {
+        request(GLOBAL.SERVER + "/hospital/getAll/全部/全部", '', "GET").then(data => {
             resolve(data)
         })
     })
 }
 
-HospitalListSvc.prototype.getLocation = function (){
+HospitalListSvc.prototype.getLocation = function() {
     return new Promise((resolve, reject) => {
         wx.getLocation({
             type: 'wgs84',
             success: function(res) {
                 var latitude = res.latitude //纬度
                 var longitude = res.longitude //经度
-                request(GLOBAL.SERVER + "/hospital/getAll/全部/全部?lng=" + longitude + "&lat=" + latitude, '' ,"GET").then(data => {
+                request(GLOBAL.SERVER + "/hospital/getAll/全部/全部?lng=" + longitude + "&lat=" + latitude, '', "GET").then(data => {
                     resolve(data)
-                 })
+                })
             },
             fail: function(err) {
-                console.log('geterr')
-                // reject(err)
+                //console.log('geterr')
+                reject(err)
             }
         })
     })
