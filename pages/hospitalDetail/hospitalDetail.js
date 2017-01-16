ae57bd534e5c420f6db8380b9b9f4be3435a0371
崔青杰 <guoxuan@kakuxuandeMac-mini.local>
2017-01-16 14:18:29 +0800

    update

--- pages/hospitalDetail/hospitalDetail.js
+++ pages/hospitalDetail/hospitalDetail.js
@@ -2,13 +2,35 @@ const HospitalListSvc = require('../../services/hospitalListSvc.js')
 
 Page({
     data: {
-        // hospital: { name: "", address: "", description: "", level: "" }
+        hospital: {}
+        // telArray: {name:"}
+    },
+    callTel: function(e){
+        var telArray = ["拨号"];
+        telArray.push("lll");
+        wx.showActionSheet({
+            itemList:telArray,
+            success: function(res) {
+
+            },
+            fail: function(res) {
+
+            }
+        })
+        // wx.makePhoneCall({
+        //   phoneNumber: e.currentTarget.dataset.,
+        //   success: function(res) {
+        //     // success
+        //   }
+        // })
     },
     onLoad: function (e) {
         var id = e.id;
+        id="58046c7a89f15b21e09e2a24";
         wx.showToast({ title: '加载中', icon: 'loading', duration: 10000, mask: true });
         var hospitalSvc = new HospitalListSvc();
         hospitalSvc.getHospitalByID(id).then(data => {
+            console.log(data)
             this.setData({
                 hospital: data.data.data
             })
