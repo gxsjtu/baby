5e2fcadbc6c4010bc9740cbcdc07057a6af5135d
lvbiao <lvbiao@lvbiaodeMac-mini.local>
2017-01-23 10:36:02 +0800

    update

--- pages/confinementDate/confinementDate.js
+++ pages/confinementDate/confinementDate.js
@@ -1,8 +1,37 @@
+const ConfinementDateSvc = require('../../services/confinementDateSvc.js')
+
 Page({
     data:{
-        showResult:false
+        showResult:false,
+        average:28,
+        year:'2016',
+        month:'10',
+        date:'01',
+        preBorn:'',
+        week:'',
+        fromNow:''
     },
     check:function(e){
+        wx.showToast({ title: '计算中', icon: 'loading', duration: 10000, mask: true });
+        let last = this.data.year+'-'+this.data.month+'-'+this.data.date;
+        var confinementDateSvc = new ConfinementDateSvc();
+        confinementDateSvc.getBorn(last,this.data.average).then(data=>{
+            if(data.data.message=='OK')
+            {
+                let preBorn = data.data.data.preBorn;
+                let fromNow = data.data.data.fromNow;
+                let week = data.data.data.week;
+                this.setData({
+                    preBorn:preBorn,
+                    fromNow:fromNow,
+                    week:week
+                });
+            }
+            wx.hideToast();
+        }).catch((err) => {
+            wx.hideToast();
+        });
+
         this.setData({
             showResult:true
         });
---
+++ services/confinementDateSvc.js
@@ -0,0 +1,16 @@
+const GLOBAL = require('../global.js');
+const request = require('../utils/request.js').request;
+
+var ConfinementDateSvc = function () { }
+
+ConfinementDateSvc.prototype.getBorn = function (last, average) {
+    return new Promise((resolve, reject) => {
+        request(GLOBAL.SERVER + "/hospital/calculateBorn/" + last + "/" + average, null, "GET").then(data => {
+            resolve(data);
+        }).catch(err => {
+            console.log(err);
+        });
+    })
+}
+
+module.exports = ConfinementDateSvc;
