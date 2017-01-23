c6cf1e7aa806364fb502f4ef32390300b8517abd
lvbiao <lvbiao@lvbiaodeMac-mini.local>
2017-01-23 16:58:13 +0800

    update

--- pages/confinementDate/confinementDate.js
+++ pages/confinementDate/confinementDate.js
@@ -1,41 +1,51 @@
 const ConfinementDateSvc = require('../../services/confinementDateSvc.js')
 
 Page({
-    data:{
-        showResult:false,
-        average:28,
-        year:'2016',
-        month:'12',
-        date:'31',
-        preBorn:'',
-        week:'',
-        fromNow:'',
-        start:'',
-        end:''
+    data: {
+        showResult: false,
+        averageIndex: 8,
+        year: '',
+        month: '',
+        date: '',
+        preBorn: '',
+        week: '',
+        fromNow: '',
+        start: '',
+        end: '',
+        selectDate: '',
+        averageArr: [20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45]
     },
-    onLoad:function(e){
-        let year = new Date().getFullYear();
-        let start = (year - 1)+'01-01';
-        let end = (year + 1)+'12-31';
+    onLoad: function (e) {
+        let now = new Date();
+        let year = now.getFullYear();
+        let month = now.getMonth() + 1;
+        let date = now.getDate();
+        let start = (year - 1) + '-01-01';
+        let end = (year + 1) + '-12-31';
+        let selectDate = year + (month < 10 ? '-0' + month : '-' + month) + (date < 10 ? '-0' + date : '-' + date);
         this.setData({
             start: start,
-            end: end
+            end: end,
+            year:year,
+            month:month,
+            date:date,
+            selectDate:selectDate,
+            average:this.data.averageArr[8]
         });
     },
-    check:function(e){
+    check: function (e) {
         wx.showToast({ title: '计算中', icon: 'loading', duration: 10000, mask: true });
-        let last = this.data.year+'-'+this.data.month+'-'+this.data.date;
+        let last = this.data.year + '-' + this.data.month + '-' + this.data.date;
         var confinementDateSvc = new ConfinementDateSvc();
-        confinementDateSvc.getBorn(last,this.data.average).then(data=>{
-            if(data.data.message=='OK')
-            {
+        confinementDateSvc.getBorn(last, this.data.average).then(data => {
+            if (data.data.message == 'OK') {
                 let preBorn = data.data.data.preBorn;
                 let fromNow = data.data.data.fromNow;
                 let week = data.data.data.week;
                 this.setData({
-                    preBorn:preBorn,
-                    fromNow:fromNow,
-                    week:week
+                    preBorn: preBorn,
+                    fromNow: fromNow,
+                    week: week
                 });
             }
             wx.hideToast();
@@ -44,12 +54,31 @@ Page({
         });
 
         this.setData({
-            showResult:true
+            showResult: true
         });
     },
-    hiddenMask:function(e){
+    hiddenMask: function (e) {
         this.setData({
-            showResult:false
+            showResult: false
+        });
+    },
+    changeDate: function(e){
+        let select = e.detail.value;
+        let arr = select.split('-');
+        this.setData({
+            year:arr[0],
+            month:arr[1],
+            date:arr[2],
+            selectDate: select
+        });
+    },
+    changeAverage: function(e){
+        console.log(e.detail.value);
+        console.log(this.data.averageArr[e.detail.value]);
+        let index = e.detail.value;
+        this.setData({
+            averageIndex: index,
+            average: this.data.averageArr[index]
         });
     }
 })
