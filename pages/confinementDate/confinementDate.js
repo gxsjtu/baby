969d3dd787bdbb4428710dd316c9f2596b5721f3
郭轩 <guoxuan@guoxuan.local>
2017-01-24 12:13:12 +0800

    OK

--- pages/confinementDate/confinementDate.js
+++ pages/confinementDate/confinementDate.js
@@ -13,16 +13,47 @@ Page({
         start: '',
         end: '',
         selectDate: '',
-        averageArr: [20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45]
+        averageArr: [
+            20,
+            21,
+            22,
+            23,
+            24,
+            25,
+            26,
+            27,
+            28,
+            29,
+            30,
+            31,
+            32,
+            33,
+            34,
+            35,
+            36,
+            37,
+            38,
+            39,
+            40,
+            41,
+            42,
+            43,
+            44,
+            45
+        ]
     },
-    onLoad: function (e) {
+    onLoad: function(e) {
         let now = new Date();
         let year = now.getFullYear();
         let month = now.getMonth() + 1;
         let date = now.getDate();
         let start = (year - 1) + '-01-01';
         let end = (year + 1) + '-12-31';
-        let selectDate = year + (month < 10 ? '-0' + month : '-' + month) + (date < 10 ? '-0' + date : '-' + date);
+        let selectDate = year + (month < 10
+            ? '-0' + month
+            : '-' + month) + (date < 10
+            ? '-0' + date
+            : '-' + date);
         this.setData({
             start: start,
             end: end,
@@ -33,8 +64,8 @@ Page({
             average: this.data.averageArr[8]
         });
     },
-    check: function (e) {
-        wx.showToast({ title: '计算中', icon: 'loading', duration: 10000, mask: true });
+    check: function(e) {
+        wx.showToast({title: '计算中', icon: 'loading', duration: 10000, mask: true});
         // let last = this.data.year + '-' + this.data.month + '-' + this.data.date;
         let last = this.data.selectDate;
         var confinementDateSvc = new ConfinementDateSvc();
@@ -44,52 +75,28 @@ Page({
                 let preBorn = data.data.data.preBorn;
                 let fromNow = data.data.data.fromNow;
                 let week = data.data.data.week;
-                this.setData({
-                    preBorn: preBorn,
-                    fromNow: fromNow,
-                    week: week
-                });
-            }
-            else {
-                this.setData({
-                    showResult: false
-                });
-                wx.showToast({
-                    title: data.data.message,
-                    duration: 3000
-                })
+                this.setData({preBorn: preBorn, fromNow: fromNow, week: week, showResult: true});
+            } else {
+                this.setData({showResult: false});
+                wx.showToast({title: data.data.message, duration: 3000})
             }
 
         }).catch((err) => {
             wx.hideToast();
         });
-
-        this.setData({
-            showResult: true
-        });
     },
-    hiddenMask: function (e) {
-        this.setData({
-            showResult: false
-        });
+    hiddenMask: function(e) {
+        this.setData({showResult: false});
     },
-    changeDate: function (e) {
+    changeDate: function(e) {
         let select = e.detail.value;
         let arr = select.split('-');
-        this.setData({
-            year: arr[0],
-            month: arr[1],
-            date: arr[2],
-            selectDate: select
-        });
+        this.setData({year: arr[0], month: arr[1], date: arr[2], selectDate: select});
     },
-    changeAverage: function (e) {
+    changeAverage: function(e) {
         console.log(e.detail.value);
         console.log(this.data.averageArr[e.detail.value]);
         let index = e.detail.value;
-        this.setData({
-            averageIndex: index,
-            average: this.data.averageArr[index]
-        });
+        this.setData({averageIndex: index, average: this.data.averageArr[index]});
     }
 })
