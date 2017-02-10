d2edd0553bac3efb6cc650f196043825155c4506
lvbiao <lvbiao@lvbiaodeMac-mini.local>
2017-02-10 16:25:45 +0800

    update

--- pages/lessCardOtherStep1/lessCardOtherStep1.js
+++ pages/lessCardOtherStep1/lessCardOtherStep1.js
@@ -1,3 +1,11 @@
 Page({
-    
+    data:{
+        credentials:[
+            {name:'准生证',src:'../../assets/images/credential1.jpg'},
+            {name:'流动人口婚育证明',src:'../../assets/images/credential2.jpg'}
+        ]
+    },
+    next: function(e){
+        wx.navigateTo({ url: '../lessCardOtherStep2/lessCardOtherStep2' });
+    }
 })
