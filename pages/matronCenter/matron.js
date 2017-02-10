3dbd4b2a32f007e54df9c76332bb76106337c41e
lvbiao <lvbiao@lvbiaodeMac-mini.local>
2017-02-10 09:44:32 +0800

    update

--- pages/matronCenter/matron.js
+++ pages/matronCenter/matron.js
@@ -1,3 +1,12 @@
 Page({
-    
+    call: function (e) {
+        let phone = e.target.dataset.phone;
+        if (phone) {
+            wx.makePhoneCall({
+                phoneNumber: phone, success: function (res) {
+                    // success
+                }
+            })
+        }
+    }
 })
