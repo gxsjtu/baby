21cccd7897ed7224137ff29caa9e0ce30fd36e9d
郭轩 <guoxuan@guoxuan.local>
2017-02-10 10:38:46 +0800

    OK

---
+++ services/locationSvc.js
@@ -0,0 +1,10 @@
+const Promise = require('../utils/promise.min.js');
+const xml2json = require('../utils/xml2json.js');
+
+var LocationSvc = function() {}
+
+LocationSvc.prototype.getDistricts = function () {
+
+};
+
+module.exports = LocationSvc;
