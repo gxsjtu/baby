96141d9c6f6272cb2893477eb4c9a225e3ad703c
lvbiao <lvbiao@lvbiaodeMac-mini.local>
2017-02-14 15:23:02 +0800

    update

--- pages/userInfo/userInfo.js
+++ pages/userInfo/userInfo.js
@@ -1,3 +1,53 @@
 Page({
-    
+    data: {
+        btnDisabled:false,
+        confinementDate:'',
+        phone:'',
+        census:'',
+        currentAddress:'',
+        hospitalName:'',
+        doctor:''
+    },
+    onLoad: function (e) {
+        let globalData = getApp().globalData;
+        console.log(globalData);
+        if(!globalData.hospital)
+        {
+            globalData.hospital = globalData.defaultHos;
+        }
+        this.setData({ userInfo: globalData.userInfo });
+        if(globalData.hospital)
+        {
+            this.setData({
+                hospitalName:globalData.hospital.name
+            });
+        }
+        if(globalData.user.preBorn){
+            this.setData({
+                confinementDate:globalData.user.preBorn
+            });
+        }
+        if(globalData.user.address && globalData.user.address.juZhu)
+        {   
+            let juzhu = globalData.user.address.juZhu;
+            this.setData({
+                currentAddress:juzhu.district+juzhu.street+juzhu.detail
+            });
+        }
+    },
+    saveInfo:function(e){
+
+    },
+    setConfinementDate:function(e){
+
+    },
+    setPhone:function(e){
+
+    },
+    setCensus:function(e){
+        
+    },
+    setCurrentAddress:function(e){
+        
+    }
 })
