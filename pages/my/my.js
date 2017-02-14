d8343e4f4a9f5067dad6da7b575da833ead4fbe6
lvbiao <lvbiao@lvbiaodeMac-mini.local>
2017-02-14 11:48:20 +0800

    update

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,3 +1,12 @@
 Page({
-    
+    data: {
+        
+    },
+    onLoad: function (e) {
+        let globalData = getApp().globalData;
+        this.setData({ userInfo: globalData.userInfo });
+    },
+    gotoInfo:function(e){
+        wx.navigateTo({ url: '../userInfo/userInfo' });
+    }
 })
