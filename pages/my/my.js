2e274a6e95efbec3effe87858554990de16d4287
lvbiao <lvbiao@lvbiaodeMac-mini.local>
2017-02-15 09:31:10 +0800

    update

--- pages/my/my.js
+++ pages/my/my.js
@@ -8,5 +8,9 @@ Page({
     },
     gotoInfo:function(e){
         wx.navigateTo({ url: '../userInfo/userInfo' });
+    },
+    gotoWait:function(e){
+        console.log(e);
+        wx.navigateTo({ url: '../wait/wait?name='+e.currentTarget.dataset.menuname });
     }
 })
