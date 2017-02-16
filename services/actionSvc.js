af2c57d88847525d0eb403e81c52b8259fe43c26
郭轩 <guoxuan@guoxuan.local>
2017-02-16 15:39:14 +0800

    OK

---
+++ services/actionSvc.js
@@ -0,0 +1,9 @@
+const GLOBAL = require('../global.js');
+const request = require('../utils/request.js').request;
+const Promise = require('../utils/promise.min.js');
+
+var ActionSvc = function() {};
+
+ActionSvc.prototype.clickGood = function(hospitalId, pageId) {};
+
+ActionSvc.prototype.getModal = function(pageId) {};
