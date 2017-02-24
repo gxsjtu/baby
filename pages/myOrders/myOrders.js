dcf22d10f3ba9bc31d99bda466fde6d150279af2
崔青杰 <guoxuan@kakuxuandeMac-mini.local>
2017-02-24 15:53:39 +0800

    update

--- pages/myOrders/myOrders.js
+++ pages/myOrders/myOrders.js
@@ -4,36 +4,43 @@ const GLOBAL = require('../../global.js');
 Page({
     data: {
         orderList: [],
-        imgAddress: GLOBAL.SERVER + "/images/"
+        imgAddress: GLOBAL.SERVER + "/images/",
+        isNoRecord: true
     },
     onLoad: function () {
         var orderSvc = new OrderSvc();
         orderSvc.getOrders().then(data => {
             var dataList = data.data.data;
-            var newOrderList = [];
-            for (var i = 0; i < dataList.length; i++) {
-                var imgUrlList = [];
-                var order = { id: dataList[i]._id, date: dataList[i].date, name: dataList[i].name, items: dataList[i].items };
-                var itemsName = "";
-                for (var j = 0; j < dataList[i].items.length; j++) {
-                    if (j == 0) {
-                        itemsName += dataList[i].items[j].name;
-                    } else {
-                        itemsName += "、" + dataList[i].items[j].name;
-                    }
-
-                    var url = this.data.imgAddress + "bundles/" + order.id + "/" + dataList[i].items[j].name + "/" + dataList[i].items[j].name;
-                    imgUrlList.push(encodeURI(url));
-                    // imgUrlList.push(url);
-                }
-                order.itemsName = itemsName;
-                order.imgUrlList = imgUrlList;
-                newOrderList.push(order);
+            if (dataList == null || dataList == undefined || dataList.length <= 0) {
+                this.setData({ isNoRecord: true});
             }
+            else {
+                var newOrderList = [];
+                for (var i = 0; i < dataList.length; i++) {
+                    var imgUrlList = [];
+                    var order = { id: dataList[i]._id, date: dataList[i].date, name: dataList[i].name, items: dataList[i].items };
+                    var itemsName = "";
+                    for (var j = 0; j < dataList[i].items.length; j++) {
+                        if (j == 0) {
+                            itemsName += dataList[i].items[j].name;
+                        } else {
+                            itemsName += "、" + dataList[i].items[j].name;
+                        }
 
-            this.setData({
-                orderList: newOrderList
-            })
+                        var url = this.data.imgAddress + "bundles/" + order.id + "/" + dataList[i].items[j].name + "/" + dataList[i].items[j].name;
+                        imgUrlList.push(encodeURI(url));
+                        // imgUrlList.push(url);
+                    }
+                    order.itemsName = itemsName;
+                    order.imgUrlList = imgUrlList;
+                    newOrderList.push(order);
+                }
+                this.setData({
+                    orderList: newOrderList,
+                    isNoRecord: false
+                })
+            }
         })
     }
+
 })
