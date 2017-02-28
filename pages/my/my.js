b70ce328b7b3443686fbcd14eec256319965f98b
郭轩 <guoxuan@guoxuan.local>
2017-02-28 15:16:11 +0800

    OK

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,26 +1,25 @@
 Page({
-    data: {
-
+    data: {},
+    onShareAppMessage: function() {
+        return {title: '事妈驾到', path: '/pages/index/index'}
     },
-    onLoad: function (e) {
+    onLoad: function(e) {
         let globalData = getApp().globalData;
-        this.setData({ userInfo: globalData.userInfo });
+        this.setData({userInfo: globalData.userInfo});
     },
-    gotoInfo: function (e) {
-        wx.navigateTo({ url: '../userInfo/userInfo' });
+    gotoInfo: function(e) {
+        wx.navigateTo({url: '../userInfo/userInfo'});
     },
-    gotoWait: function (e) {
+    gotoWait: function(e) {
         console.log(e);
-        wx.navigateTo({ url: '../wait/wait?name=' + e.currentTarget.dataset.menuname });
-    },
-    gotoAchievement: function (e) {
         wx.navigateTo({
-            url: '../myAchievement/myAchievement'
+            url: '../wait/wait?name=' + e.currentTarget.dataset.menuname
         });
     },
-    gotoMyOrders: function (e) {
-        wx.navigateTo({
-            url: '../myOrders/myOrders'
-        });
+    gotoAchievement: function(e) {
+        wx.navigateTo({url: '../myAchievement/myAchievement'});
+    },
+    gotoMyOrders: function(e) {
+        wx.navigateTo({url: '../myOrders/myOrders'});
     }
 })
